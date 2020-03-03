500c7b100bb7703eb8fe146e37a8b888f199cbf9
Kona <kitami.kitano20@gmail.com>
2020-03-03 18:53:26 +0900

    効果追加

--- js/index.js
+++ js/index.js
@@ -5,3 +5,22 @@ ityped.init(document.querySelector("#ityped"), {
   typeSpeed: 140,
   loop: false
 });
+
+const jin = document.getElementById('jin');
+const hotaru = document.getElementById('hotaru');
+const logo = document.getElementById('logo');
+
+
+jin.addEventListener('mouseenter', () => {
+  logo.style.transform = "translateX(-180px)";
+}, false);
+jin.addEventListener('mouseleave', () => {
+  logo.style.transform = "translateX(0)";
+}, false);
+
+hotaru.addEventListener('mouseenter', () => {
+  logo.style.transform = "translateX(205px)";
+}, false);
+hotaru.addEventListener('mouseleave', () => {
+  logo.style.transform = "translateX(0)";
+}, false);
